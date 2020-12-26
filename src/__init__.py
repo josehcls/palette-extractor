3b94d3d5342b47876d3c5ee50e92f98356ce429e
Jose Leopoldo <josehcls@gmail.com>
2020-12-26 12:36:25 -0300

    results and orderingcolors

--- src/__init__.py
+++ src/__init__.py
@@ -4,32 +4,39 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
 import math
+import colorsys
 
-img = cv2.imread('../images/kagemusha.jpg')
+img_path = '../images/rivendell.jpg'
+clusters = 10
+debug = False
+
+# img = cv2.imread('../images/valley.jpg')
+img = cv2.imread(img_path)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 plt.imshow(img)
 # plt.show()
 
-clusters = 10
 
 #####
+# Considerando cada cor 
 
-# array = np.zeros([256,256,256])
+if (debug):
+    array = np.zeros([256,256,256])
 
-# for row in range(img.shape[0]):
-#     for column in range(img.shape[1]):
-#         pixel = img[row][column]
-#         array[pixel[0]][pixel[1]][pixel[2]] = array[pixel[0]][pixel[1]][pixel[2]] + 1
+    for row in range(img.shape[0]):
+        for column in range(img.shape[1]):
+            pixel = img[row][column]
+            array[pixel[0]][pixel[1]][pixel[2]] = array[pixel[0]][pixel[1]][pixel[2]] + 1
 
-# z,x,y = array.nonzero()
-# colors = list(zip(z/255, x/255, y/255))
-# weight = [ 10*math.log(array[i[0]][i[1]][i[2]]) for i in list(zip(z, x, y))]
+    z,x,y = array.nonzero()
+    colors = list(zip(z/255, x/255, y/255))
+    weight = [ 10*math.log(array[i[0]][i[1]][i[2]]) for i in list(zip(z, x, y))]
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(z, x, y, zdir='z', c=colors, s=weight)
-# plt.show()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(z, x, y, zdir='z', c=colors, s=weight)
+    plt.show()
 
 # kmeans = KMeans(
 #     n_clusters=clusters,
@@ -55,8 +62,9 @@ clusters = 10
 # ax.scatter(z, x, y, zdir='z', c=colors_,)
 # plt.show()
 
-# Considerar peso criando multiplos pontos ??
 
+
+# Considerando cada pixel 
 
 width = img.shape[0]
 heigth = img.shape[1]
@@ -72,10 +80,11 @@ for i in range(width):
 
 colors = list(zip(np.array(r)/255, np.array(g)/255, np.array(b)/255))
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(r, g, b, zdir='r', c=colors)
-# plt.show()
+if (debug):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(r, g, b, zdir='r', c=colors)
+    plt.show()
 
 kmeans = KMeans(
     n_clusters=clusters,
@@ -98,12 +107,42 @@ for i in range(len(centroids)):
 
 clustered_colors = [available_colors[i] for i in labels]
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(r, g, b, zdir='r', c=clustered_colors,)
-# plt.show()
+if (debug):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(r, g, b, zdir='r', c=clustered_colors,)
+    plt.show()
+
+plt.imshow(img)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.bar(x=list(range(len(centroids))), height=[1] * len(centroids),  color=[np.array(centroid, int)/255 for centroid in centroids])
 plt.show()
+
+## Ordenando cores
+
+ordered_centroids = list(centroids)
+ordered_centroids.sort(key=lambda rgb: colorsys.rgb_to_hls(*rgb))
+centroids = ordered_centroids
+
+##
+
+## OUTPUT
+
+result = ''
+
+with open("output_template.html", "r") as template:
+    result = template.read()
+
+result = result.replace('@image', img_path)
+result = result.replace('@width_plus_padding', str(img.shape[1] + 10))
+content = [f'<td style="height: {int(0.40 * img.shape[0])};  background-color: rgb{int(c[0]), int(c[1]), int(c[2])};"></td>' for c in centroids]
+result = result.replace('@content', ''.join(content))
+
+with open("../output/output.html", "w") as output:
+    output.truncate()
+    output.write(result)
+
+##
+
